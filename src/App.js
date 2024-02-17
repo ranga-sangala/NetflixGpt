58dc388621c228369c14ebf759ade19a710c3dc5
ranga-sangala <rangasangala7@gmail.com>
2024-02-17 14:48:15 +0530

    CRA and Tailwind configured

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl text-green-800'>Hello Ranga</h1>
+    </div>
+  );
+}
+
+export default App;
